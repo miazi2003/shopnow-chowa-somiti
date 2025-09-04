482665e6278e2fc51dd406f7e44ce2765c00218e
miazi2003 <yeasinmiazi1997@gmail.com>
2025-09-04 23:50:52 +0600

    banner added

---
+++ src/component/Banner/Banner.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import banner from "../../assets/Banner.png";
+const Banner = () => {
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: `url(${banner})`,
+      }}
+    >
+      <div className="hero-overlay"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w">
+          <h1 className="mb-5 text-4xl font-bold">
+            সঞ্চয়ে সমৃদ্ধি করি , একসাথে ভবিষ্যৎ গড়ি
+          </h1>
+          <p className="mb-5">
+            আমাদের সঞ্চয় সমিতির লক্ষ্য হলো নিরাপদ সঞ্চয়ের মাধ্যমে আর্থিক
+            স্বাবলম্বিতা ও পারস্পরিক সহযোগিতা নিশ্চিত করা। <br/> আজই যুক্ত হোন এবং
+            আপনার আগামীকালকে আরও সুরক্ষিত করুন।
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
